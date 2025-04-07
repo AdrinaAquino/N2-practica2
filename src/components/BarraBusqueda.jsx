29531fe63dd11a0bb15d707f7eef08c65e47ed1c
adrianasheila <adrisheiaquiblan@gmail.com>
2025-04-07 01:38:53 -0400

    buscador y responsive

--- src/components/BarraBusqueda.jsx
+++ src/components/BarraBusqueda.jsx
@@ -1,18 +1,22 @@
+import { useState } from "react"
 
 
-export default function BarraBusqueda({searchValue,setSearchValue}) {
-    
-    const palabraBuscar=(e)=>{setSearchValue(e.target.value)}
-console.log(setSearchValue)
+export default function BarraBusqueda({ searchValue, setSearchValue, handClick }) {
+  const [update, setUpdate] = useState()
+
+  const buttonOnclick = () => {
+    setUpdate(searchValue)
+  }
   return (
-      <form action="" className='flex justify-center  p-0 mt-8 mb-8' >
-        <input type="text" name='search' placeholder='Search...' className=' w-100  p-2 border-3 border-gray-300 border-r-0 rounded-l-md ' value={searchValue} onChange={palabraBuscar}/>
-        <button className="border-3 border-gray-300 rounded-r-md"
-        >
-          <svg height="32" width="32">
-            <path d="M19.427 21.427a8.5 8.5 0 1 1 2-2l5.585 5.585c.55.55.546 1.43 0 1.976l-.024.024a1.399 1.399 0 0 1-1.976 0l-5.585-5.585zM14.5 21a6.5 6.5 0 1 0 0-13 6.5 6.5 0 0 0 0 13z" fill="#000000" fillRule="evenodd"></path>
-          </svg>
-        </button>
-      </form>
+    <form action="" className='flex justify-center  p-0 mt-8 mb-8' >
+      <input type="text" name='search' placeholder='Search...' className=' w-100  p-2 border-3 border-gray-300 border-r-0 rounded-l-md ' value={update} onChange={handClick} />
+      <button className="border-3 border-gray-300 rounded-r-md"
+        onClick={buttonOnclick}
+      >
+        <svg height="32" width="32">
+          <path d="M19.427 21.427a8.5 8.5 0 1 1 2-2l5.585 5.585c.55.55.546 1.43 0 1.976l-.024.024a1.399 1.399 0 0 1-1.976 0l-5.585-5.585zM14.5 21a6.5 6.5 0 1 0 0-13 6.5 6.5 0 0 0 0 13z" fill="#000000" fillRule="evenodd"></path>
+        </svg>
+      </button>{update}
+    </form>
   )
 }
--- src/components/NombreBusqueda.jsx
+++ src/components/NombreBusqueda.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NombreBusqueda({ searchValue }) {
     return (
         <div>
-            <h2 className='text-center text-3xl'>{searchValue} Pictures</h2>)
+            <h2 className='text-center text-3xl'>{searchValue} Pictures</h2>
         </div>
     )
 }
